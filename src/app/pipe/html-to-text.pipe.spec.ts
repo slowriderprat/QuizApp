747dcd120339ab42046affaa10f30192366d8709
SRIVASTAVA Pratik <pratik.srivastava@soprasteria.com>
2023-08-15 11:44:40 +0530

    Auto-generated commit [ci skip]

--- src/app/pipe/html-to-text.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HtmlToTextPipe } from './html-to-text.pipe';
-
-describe('HtmlToTextPipe', () => {
-  it('create an instance', () => {
-    const pipe = new HtmlToTextPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
